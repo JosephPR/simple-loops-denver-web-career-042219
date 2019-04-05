4f1f9dc87c2e685d7ae547416c80a6586418ab74
Joseph Reilly <reilly4181@gmail.com>
2019-04-05 17:09:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,9 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
- loop do
-   puts
-end
+
 end
 
 def times_iterator(number_of_times)
@@ -13,9 +11,11 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0 
+ while counter < number_of_times
+ puts "Welcome to Flatiron School's Web Development Course!"
+  counter += 1 
+end
   
 end
 
