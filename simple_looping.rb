550cc734daea0ab8782a26951cd0824391ef6d41
Joseph Reilly <reilly4181@gmail.com>
2019-04-05 17:27:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,9 +12,9 @@ end
 end
 
 def times_iterator(number_of_times)
-7.times do 
-puts "Welcome to Flatiron School's Web Development Course!"
-end
+# 7.times do 
+# puts "Welcome to Flatiron School's Web Development Course!"
+# end
 end
 
 def while_iterator(number_of_times)
