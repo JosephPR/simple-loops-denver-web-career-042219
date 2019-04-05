9ae0a9e67456c781856979297dd2fa48e9959129
Joseph Reilly <reilly4181@gmail.com>
2019-04-05 17:24:20 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,8 @@ counter = 0
  loop do 
    counter = counter + 1
 puts "Welcome to Flatiron School's Web Development Course!"
-  
+    if counter >= number_of_times # If our counter is 10 or more
+    break # Stop the loop
   end
 end
 
