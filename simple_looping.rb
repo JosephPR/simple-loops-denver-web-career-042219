3e45130babf2fd0199cc30f17020991740dda7e6
Joseph Reilly <reilly4181@gmail.com>
2019-04-05 17:31:31 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,7 @@ end
 end
 
 def for_iterator(number_of_times)
-for phrase in number_of_times
+for each in number_of_times
     puts  "Welcome to Flatiron School's Web Development Course!"
 end
 end
